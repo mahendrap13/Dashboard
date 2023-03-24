e7c10c3583dfa54b9664d5878740eb228df3fff8
Mahendra Parihar <106741843+mahendrap13@users.noreply.github.com>
2023-03-24 18:28:43 +0530

    Add files via upload

---
+++ MainFolder/script.js
@@ -0,0 +1,489 @@
+function fun(a) {
+  //   alert("jabyv")
+  // document.getElementById("drop1").style="display: block !important;  transition: all 0.5s linear;"
+
+  if (a.childNodes[3].id == "drop1") {
+    document.getElementById("drop1").style =
+      "display: block !important;  transition: all 0.5s linear;";
+    document.getElementById("drop2").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop4").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop5").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop6").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop7").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: none !important;  transition: all 0.5s linear;";
+
+    document.getElementById("drop3").style =
+      "display: none !important;  transition: all 0.5s linear;";
+  } else if (a.childNodes[3].id == "drop2") {
+    document.getElementById("drop1").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop3").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop4").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop5").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop6").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop7").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: none !important;  transition: all 0.5s linear;";
+
+    document.getElementById("drop2").style =
+      "display: block !important;  transition: all 0.5s linear;";
+  } else if (a.childNodes[3].id == "drop3") {
+    document.getElementById("drop1").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop2").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop4").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop5").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop6").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop7").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: none !important;  transition: all 0.5s linear;";
+
+    document.getElementById("drop3").style =
+      "display: block !important;  transition: all 0.5s linear;";
+  } else if (a.childNodes[3].id == "drop4") {
+    document.getElementById("drop1").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop2").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop3").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop5").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop6").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop7").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: none !important;  transition: all 0.5s linear;";
+
+    document.getElementById("drop4").style =
+      "display: block !important;  transition: all 0.5s linear;";
+  } else if (a.childNodes[3].id == "drop5") {
+    document.getElementById("drop1").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop2").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop3").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop4").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop6").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop7").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: none !important;  transition: all 0.5s linear;";
+
+    document.getElementById("drop5").style =
+      "display: block !important;  transition: all 0.5s linear;";
+  } else if (a.childNodes[3].id == "drop6") {
+    document.getElementById("drop1").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop2").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop3").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop4").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop5").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop7").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: none !important;  transition: all 0.5s linear;";
+
+    document.getElementById("drop6").style =
+      "display: block !important;  transition: all 0.5s linear;";
+  } else if (a.childNodes[3].id == "drop7") {
+    document.getElementById("drop1").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop2").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop3").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop4").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop5").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop6").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: none !important;  transition: all 0.5s linear;";
+
+    document.getElementById("drop7").style =
+      "display: block !important;  transition: all 0.5s linear;";
+  } else if (a.childNodes[5].id == "drop8") {
+    document.getElementById("drop1").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop2").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop3").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop4").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop5").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop6").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop7").style =
+      "display: none !important;  transition: all 0.5s linear;";
+    document.getElementById("drop8").style =
+      "display: block !important;  transition: all 0.5s linear;";
+  }
+}
+// ==================================nav and tabs for nav drop===============================
+var tab1 = document.getElementById("tab1");
+var b = document.getElementById("tab2");
+var c = document.getElementById("tab3");
+
+var bt1 = document.getElementById("bt1");
+var bt2 = document.getElementById("bt2");
+var bt3 = document.getElementById("bt3");
+function tabs(a) {
+  if (a.innerHTML == "Messages") {
+    tab1.style.display = "block";
+    b.style.display = "none";
+    c.style.display = "none";
+    bt1.style = " background-color: #3f6ad8; color:white";
+    bt2.style = "  background-color: white; color:black;";
+    bt3.style = "  background-color: white; color:black;";
+    // alert("kk")
+  } else if (a.innerHTML == "Events") {
+    tab1.style.display = "none";
+    c.style.display = "none";
+    b.style.display = "block";
+    bt2.style = " background-color: #3f6ad8; color:white";
+    bt1.style = "  background-color: white; color:black;";
+    bt3.style = "  background-color: white; color:black;";
+  } else if (a.innerHTML == "Stystem Errors") {
+    tab1.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "block";
+    bt3.style = " background-color: #3f6ad8; color:white";
+    bt1.style = "  background-color: white; color:black;";
+    bt2.style = "  background-color: white; color:black;";
+  }
+}
+// ================================== in nav for change city===============================
+
+function city(city) {
+  document.getElementById("cityimg").src = city.childNodes[0].src;
+  // console.log(city.childNodes[0]);
+}
+
+// ================================== in nav for menu bar ==============================
+
+document.querySelector(".menu-icon").addEventListener("click", function () {
+  let lines = document.querySelector(".menu-icon").children;
+  if (lines[0].classList.contains("active")) {
+    for (let i in lines) {
+      lines[i].classList.remove("active");
+      lines[i].classList.add("deactive");
+    }
+  } else {
+    for (let i in lines) {
+      lines[i].classList.remove("deactive");
+      lines[i].classList.add("active");
+    }
+  }
+});
+
+// ==================================drop down ===============================
+
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  // const dmenu = dropdown.querySelector(".dmenu");
+  const dmenu = dropdown.querySelector(".con");
+  const options = dropdown.querySelectorAll(".dmenu li");
+  const selected = dropdown.querySelector(".selected");
+
+  select.addEventListener("click", () => {
+    selected.classList.toggle("select-clicked");
+    caret.classList.toggle("caret-rotate");
+    // dmenu.classList.toggle('dmenu-open');
+    dmenu.classList.toggle("con-open");
+  });
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      // selected.innerHTML = option.innerHTML;
+      selected.classList.remove("select-checked");
+      caret.classList.remove("caret-rotate");
+      // dmenu.classList.remove("dmenu-open");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      option.classList.add("active");
+    });
+  });
+});
+
+// ==================================progressbar circle ===============================
+
+let circularprogress0 = document.querySelector(".circular-progressbar0");
+let progressvalue0 = document.querySelector(".progress-value0");
+
+let progressStratValue0 = 0,
+  progressEndValue0 = 51,
+  speed0 = 100;
+let progress0 = setInterval(() => {
+  progressStratValue0++;
+  progressvalue0.textContent = `${progressStratValue0}%`;
+  circularprogress0.style.background = `conic-gradient(#FDA42D ${
+    progressStratValue0 * 3.6
+  }deg, #E5E5E5 0deg)`;
+  if (progressStratValue0 == progressEndValue0) {
+    clearInterval(progress0);
+  }
+}, speed0);
+
+let circularprogress1 = document.querySelector(".circular-progressbar1");
+let progressvalue1 = document.querySelector(".progress-value1");
+
+let progressStratValue1 = 0,
+  progressEndValue1 = 16,
+  speed1 = 100;
+let progress1 = setInterval(() => {
+  progressStratValue1++;
+  progressvalue1.textContent = `${progressStratValue1}%`;
+  circularprogress1.style.background = `conic-gradient(#3AC47D ${
+    progressStratValue1 * 3.6
+  }deg, #E5E5E5 0deg)`;
+  if (progressStratValue1 == progressEndValue1) {
+    clearInterval(progress1);
+  }
+}, speed1);
+let circularprogress2 = document.querySelector(".circular-progressbar2");
+let progressvalue2 = document.querySelector(".progress-value2");
+
+let progressStratValue2 = 0,
+  progressEndValue2 = 51,
+  speed2 = 100;
+let progress2 = setInterval(() => {
+  progressStratValue2++;
+  progressvalue2.textContent = `${progressStratValue2}%`;
+  circularprogress2.style.background = `conic-gradient(#FF463A ${
+    progressStratValue2 * 3.6
+  }deg, #E5E5E5 0deg)`;
+  if (progressStratValue2 == progressEndValue2) {
+    clearInterval(progress2);
+  }
+}, speed2);
+
+// =====================delete function ========================
+
+function delete1(del) {
+  // console.log()
+  del.parentNode.parentNode.remove();
+}
+
+// ------------------------// for right toggle --------------------------------------
+
+let menubar = document.getElementById("rightToggle");
+
+function inout() {
+  menubar.classList.toggle("active3");
+}
+function out() {
+  menubar.classList.toggle("active3");
+}
+
+// ===========================nav bar ========================
+
+let navbar = document.getElementById("menuw");
+let dropdown = document.getElementById("left-div1");
+
+function mainbar() {
+  navbar.classList.toggle("active2");
+  dropdown.classList.toggle("mahi");
+}
+
+// ===================================fixed nav bar====================================
+function butto(fix) {
+  fix.childNodes[1].classList.toggle("fixed").innerHTML = "On";
+
+  if (fix.childNodes[1].innerHTML == "off") {
+    fix.childNodes[1].innerHTML = "On";
+  } else {
+    fix.childNodes[1].innerHTML = "off";
+  }
+}
+// function butto()
+
+// function butto()
+
+// ===============================================nav and dorpdown color ===========================================
+let nav = document.getElementById("nav-color");
+function color(colors) {
+  nav.style.backgroundColor = colors.value;
+}
+function navres() {
+  nav.style.backgroundColor = "#FFFFFF";
+}
+let drop = document.getElementById("left-div1");
+function colorDrop(colordrop) {
+  let dropd = document.getElementsByClassName("select");
+  drop.style.backgroundColor = colordrop.value;
+  for (var i = 0; i < dropd.length; i++) {
+    dropd[i].style.backgroundColor = colordrop.value;
+    dropd[i].classList.add("mahie");
+  }
+}
+function dropres() {
+  let dropd1 = document.getElementsByClassName("select");
+  drop.style.backgroundColor = "#FFFFFF";
+  for (var o = 0; o < dropd1.length; o++) {
+    dropd1[o].style.backgroundColor = "#FFFFFF";
+  }
+}
+// ------------------------// for ssetting coloer --------------------------------------
+
+let righsstting = document.getElementById("setting-color");
+let btnsstting = document.getElementById("colorbtn");
+
+function ssetting() {
+  righsstting.classList.toggle("settings");
+  btnsstting.classList.toggle("btn21");
+}
+
+// ===========================================add tasks==================================
+
+var taskinner = document.getElementById("addvalue");
+var taskmain = document.getElementById("taskss");
+taskinner.addEventListener("click", () => {
+  var taskvalue1 = document.getElementById("taskvalue1").value;
+  var taskvalue2 = document.getElementById("taskvalue2").value;
+  var div1 = document.createElement("div");
+  div1.className = "task after1";
+  taskmain.appendChild(div1);
+  var check = document.createElement("div");
+  check.className = "checkbox";
+  div1.appendChild(check);
+  var input = document.createElement("input");
+  input.type = "checkbox";
+  check.appendChild(input);
+  var div2 = document.createElement("div");
+  div2.className = "texts";
+  check.appendChild(div2);
+  var b = document.createElement("b");
+  b.id = "firstvalue";
+  b.innerHTML = taskvalue1;
+  div2.appendChild(b);
+  var p = document.createElement("p");
+  p.id = "secondvalue";
+  p.innerHTML = "By " + taskvalue2;
+  div2.appendChild(p);
+  var lable = document.createElement("lable");
+  p.appendChild(lable);
+  lable.className = "addlable";
+  lable.innerHTML = "NEW-LatestTask";
+  var butons = document.createElement("div");
+  butons.className = "butons";
+  div1.appendChild(butons);
+  butons.innerHTML =
+    `${'<i class="fa fa-check" aria-hidden="true"></i>'}` +
+    `${'<i onclick="delete1(this)" class="fa fa-trash" aria-hidden="true"></i>'}`;
+
+  addtaskbtn.style.display = "none";
+});
+
+// var task = document.getElementById("tasklistA");
+
+var addtaskbtn = document.getElementById("addtaskss");
+// task.addEventListener("click", () => {
+//   addtaskbtn.style.display = "flex";
+// });
+
+function addmsg() {
+  var addtaskss = document.getElementById("addtaskss");
+
+  // alert("hello")
+  addtaskss.style = "display:flex;";
+}
+
+// =====================================variation funcatin===================================
+function vari(vari) {
+  var vari2 = document.getElementById("right-div2");
+  var varibtn = document.getElementById("varicolor2");
+  console.log((vari.parentNode.parentNode.style.display = "none"));
+  vari2.style.display = "block";
+  varibtn.style.backgroundColor = " #3f6ad8 ";
+  varibtn.style.color = " white";
+}
+function vari1(vari1) {
+  var varibtn = document.getElementById("varicolor1");
+  var vari3 = document.getElementById("right-div1");
+  console.log((vari1.parentNode.parentNode.style.display = "none"));
+  vari3.style.display = "block";
+  varibtn.style.backgroundColor = " #3f6ad8";
+  varibtn.style.color = " white";
+}
+
+// ======================================Animation=============================
+gsap.fromTo(".anime", { x: -100 }, { x: 0, duration: 0.7 });
+gsap.fromTo(".brt", { x: -100 }, { x: 0, duration: 0.7 });
+
+// ======================================for caurosal ======================
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  //CHANGE SLIDE CLICK BUTTON
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n)); //CURRENT SLIDE
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides1");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// =====================================seaech==============================
+var searchin = document.getElementById("searchin");
+var inputsearch = document.getElementById("inputsearch");
+var icona = document.getElementById("icona");
+var mainu = document.getElementById("mainu");
+var crosss = document.getElementById("crosss");
+function search1(zoom) {
+  searchin.classList.toggle("zoom");
+  inputsearch.classList.toggle("zoom");
+  icona.classList.toggle("zoom");
+  mainu.classList.toggle("zoom");
+  crosss.classList.toggle("zoom");
+}
